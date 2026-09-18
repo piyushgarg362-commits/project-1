58f919257059c7af1ac721b896e62f116587859f
piyushgarg362-commits <piyushgarg362@gmail.com>
2026-09-17 21:23:13 -0400

    Create App.java

---
+++ src/main/java/com/devops/App.java
@@ -0,0 +1,14 @@
+package com.devops;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("=================================");
+        System.out.println(" DevOps Maven Application");
+        System.out.println("=================================");
+        System.out.println("Application started successfully!");
+        System.out.println("Built using Maven");
+        System.out.println("Containerized using Docker");
+        System.out.println("Built automatically using Jenkins");
+    }
+}
